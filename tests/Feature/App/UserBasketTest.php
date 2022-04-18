eaaf43f5321b540b5798d26b883de2bcbfc1d714
Nizigama jean davy <jedavy.n@gmail.com>
2022-04-18 12:07:34 +0200

    Created test for removing product from cart

--- tests/Feature/App/UserBasketTest.php
+++ tests/Feature/App/UserBasketTest.php
@@ -2,33 +2,33 @@
 
 namespace Tests\Feature\App;
 
+use App\Enum\BasketStatusID;
 use App\Models\Basket;
 use App\Models\BasketStatus;
 use App\Models\Product;
 use App\Models\User;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Auth;
 use Laravel\Sanctum\Sanctum;
 use Tests\TestCase;
 
 /** This test class is for testing features like adding, removing products from cart and to cart */
 class UserBasketTest extends TestCase
 {
-    use RefreshDatabase;
-    /**
-     * This function tests that a logged in user can add a product to the cart
-     *
-     * @test
-     */
-    public function a_logged_in_user_can_add_a_product_to_the_cart(): void
-    {
-        // arrange
+
+    use DatabaseMigrations;
+
+    protected function setUp(): void{
+        parent::setUp();
+
         $user = User::factory()->create([
             "email" => "jedavy.n@gmail.com"
         ]);
         Sanctum::actingAs($user);
         Product::factory(38)->create();
-        $addedStatus = BasketStatus::factory()->create([
+        BasketStatus::factory()->create([
             "title" => "added"
         ]);
         BasketStatus::factory()->create([
@@ -37,6 +37,15 @@ class UserBasketTest extends TestCase
         BasketStatus::factory()->create([
             "title" => "bought"
         ]);
+    }
+    /**
+     * This function tests that a logged in user can add a product to the cart
+     *
+     * @test
+     */
+    public function a_logged_in_user_can_add_a_product_to_the_cart(): void
+    {
+        // arrange
         $productID = Product::inRandomOrder()->first()->id;
         $itemsCount = 3;
 
@@ -51,9 +60,43 @@ class UserBasketTest extends TestCase
         // assert
         $response->assertStatus(200);
         $response->assertExactJson(["message" => "Product successfully added to cart"]);
-        $cartAddition = Basket::where([["userID", $user->id], ["productID", $productID], ["statusID", $addedStatus->id]])->first();
+        $cartAddition = Basket::where([["userID", Auth::id()], ["productID", $productID], ["statusID", BasketStatusID::ADDED->value]])->first();
         $this->assertModelExists($cartAddition);
-        $this->assertCount(1, Basket::where("userID", $user->id)->get());
+        $this->assertCount(1, Basket::where("userID", Auth::id())->get());
         $this->assertSame($itemsCount, $cartAddition->itemsCount);
+    }
+
+    /**
+     * This function tests that a logged in user can remove a product from the cart
+     *
+     * @test
+     */
+    public function a_logged_in_user_can_remove_a_product_from_the_cart(): void
+    {
+        // arrange
+        $productID = Product::inRandomOrder()->first()->id;
+        $itemsCount = 3;
+
+        $record = Basket::factory()->create([
+            "userID" => Auth::id(),
+            "productID" => $productID,
+            "statusID" => BasketStatusID::ADDED->value,
+            "itemsCount" => $itemsCount
+        ]);
+
+
+
+        // act
+        $response = $this->postJson("/api/app/cart/remove/$record->id");
+
+
+        // assert
+        $response->assertStatus(200);
+        $response->assertExactJson(["message" => "Product successfully removed from the cart"]);
+        $cartAddition = Basket::where([["userID", Auth::id()], ["productID", $productID], ["statusID", BasketStatusID::ADDED->value]])->first();
+        $cartRemoval = Basket::where([["userID", Auth::id()], ["productID", $productID], ["statusID", BasketStatusID::REMOVED->value]])->first();
+        $this->assertModelExists($cartAddition);
+        $this->assertModelExists($cartRemoval);
+        $this->assertCount(2, Basket::where("userID", Auth::id())->get());
     }
 }
